6420e6e7bcac477f0b2dd781910156dfe24f1019
MidnightTale <midnighttale24@gmail.com>
2024-10-14 23:38:18 +0700

    Initial commit

---
+++ src/main/kotlin/net/hynse/midnightPuddles/MidnightPuddles.kt
@@ -0,0 +1,14 @@
+package net.hynse.midnightPuddles
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MidnightPuddles : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
